6a6295f17fee0454643c1b0fb8f2fc52d51619b3
xh374 <xuhui444871403@163.com>
2015-10-21 00:55:33 +0800

    Initial Commit

---
+++ test-2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test-2
+//
+//  Created by xuhui on 15/10/21.
+//  Copyright © 2015年 xuhui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
